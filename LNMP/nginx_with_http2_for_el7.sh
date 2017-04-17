c5179aded83a48e095e48e875d9472e0ff4746d0
owent <owt5008137@live.com>
2017-04-17 10:20:30 +0800

    fix compile

--- LNMP/nginx_with_http2_for_el7.sh
+++ LNMP/nginx_with_http2_for_el7.sh
@@ -58,6 +58,7 @@ cd "$NGINX_DIR_NAME";
 ./configure \
 --prefix=/etc/nginx \
 --sbin-path=/usr/sbin/nginx \
+--modules-path=/usr/lib64/nginx/modules \
 --conf-path=/etc/nginx/nginx.conf \
 --error-log-path=/var/log/nginx/error.log \
 --http-log-path=/var/log/nginx/access.log \
@@ -70,28 +71,32 @@ cd "$NGINX_DIR_NAME";
 --http-scgi-temp-path=/var/cache/nginx/scgi_temp \
 --user=nginx \
 --group=nginx \
---with-http_ssl_module \
---with-http_realip_module \
+--with-compat \
+--with-file-aio \
+--with-threads \
 --with-http_addition_module \
---with-http_sub_module \
+--with-http_auth_request_module \
 --with-http_dav_module \
 --with-http_flv_module \
---with-http_mp4_module \
 --with-http_gunzip_module \
 --with-http_gzip_static_module \
+--with-http_mp4_module \
 --with-http_random_index_module \
+--with-http_realip_module \
 --with-http_secure_link_module \
---with-http_stub_status_module \
---with-http_auth_request_module \
---with-threads \
---with-stream \
---with-stream_ssl_module \
 --with-http_slice_module \
+--with-http_ssl_module \
+--with-http_stub_status_module \
+--with-http_sub_module \
+--with-http_v2_module \
 --with-mail \
 --with-mail_ssl_module \
---with-file-aio \
---with-http_v2_module \
---with-ipv6 \
+--with-stream \
+--with-stream_realip_module \
+--with-stream_ssl_module \
+--with-stream_ssl_preread_module \
+--with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=generic -fPIC' \
+--with-ld-opt='-Wl,-z,relro -Wl,-z,now -pie' \
 --with-openssl="$WORKING_DIR/$OPENSSL_DIR_NAME" \
 --with-openssl-opt="-fPIC" ;
 
