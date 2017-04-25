742daf97a39a18cd8c10ed2fbb21c57560327df0
owent <owt5008137@live.com>
2017-04-25 12:07:19 +0800

    reload bbr after

--- Others/centos_7_bbr_sysctl.sh
+++ Others/centos_7_bbr_sysctl.sh
@@ -8,5 +8,7 @@ sed -i "/net.ipv4.tcp_congestion_control/d" "$SYSCTL_CONF";
 echo "net.core.default_qdisc = fq
 net.ipv4.tcp_congestion_control = bbr" >> "$SYSCTL_CONF";
 
+sysctl -p $SYSCTL_CONF ;
+
 # cat /proc/sys/net/ipv4/tcp_congestion_control
 # cat /proc/sys/net/core/default_qdisc
