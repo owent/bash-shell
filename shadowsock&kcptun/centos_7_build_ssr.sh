9b8a8551f8d93fed3e4001b392d0ae161366bbf1
owent <owt5008137@live.com>
2017-07-22 21:42:24 +0800

    add file

---
+++ shadowsock&kcptun/centos_7_build_ssr.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+which yum;
+
+PREFIX="/usr/local/shadowsocksr-libev";
+MAXFD=32768;
+USER=shadowsocksr;
+
+if [ 0 -eq $? ]; then
+    sudo yum install -y gcc autoconf libtool automake make zlib-devel openssl-devel asciidoc xmlto git;
+else
+    which apt;
+    if [ 0 -eq $? ]; then
+        sudo apt install -y --no-install-recommends build-essential autoconf libtool libssl-dev libpcre3-dev asciidoc xmlto git;
+    fi
+fi
+
+if [ ! -e shadowsocksr-libev ]; then
+    git clone https://github.com/shadowsocksr/shadowsocksr-libev.git;
+    cd shadowsocksr-libev;
+else
+    cd shadowsocksr-libev;
+    git reset --hard;
+    git clean -dfx;
+    git pull;
+fi
+
+./configure  --prefix="$PREFIX" && make;
+sudo make install;
+
+cd .. ;
+
+# add systemd
+echo "#  This file is part of Shadowsocksr-libev.
+#
+#  Shadowsocksr-libev is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This file is default for Debian packaging. See also
+#  /etc/default/Shadowsocksr-libev for environment variables.
+
+[Unit]
+Description=Shadowsocksr-libev Default Server Service
+Documentation=man:Shadowsocksr-libev(8)
+After=network.target
+
+[Service]
+Type=simple
+EnvironmentFile=/etc/default/shadowsocksr-libev
+User=$USER
+Group=$USER
+LimitNOFILE=$MAXFD
+ExecStart=$PREFIX/bin/ss-server -c \$CONFFILE \$DAEMON_ARGS
+
+[Install]
+WantedBy=multi-user.target
+" > shadowsocksr-libev.service;
+
+if [ ! -e /etc/default ]; then
+    sudo mkdir -p /etc/default;
+fi
+
+# add env file
+sudo echo "# Defaults for shadowsocks initscript
+# sourced by /etc/init.d/shadowsocksr-libev
+# installed at /etc/default/shadowsocksr-libev by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+# Note: `START', `GROUP' and `MAXFD' options are not recognized by systemd.
+# Please change those settings in the corresponding systemd unit file.
+
+# Enable during startup?
+START=yes
+
+# Configuration file
+CONFFILE=\"/etc/shadowsocksr-libev/config.json\"
+
+# Extra command line arguments
+DAEMON_ARGS=\"-u\"
+
+# User and group to run the server as
+USER=$USER
+GROUP=$USER
+
+# Number of maximum file descriptors
+MAXFD=$MAXFD
+" > /etc/default/shadowsocksr-libev;
+
+# add config file
+if [ ! -e "/etc/shadowsocksr-libev/config.json" ]; then
+    sudo mkdir -p /etc/shadowsocksr-libev ;
+
+    sudo echo '{
+    "server": "0.0.0.0",
+    "server_ipv6": "[::]",
+    "local_port": 1080,
+    "port_password": {
+        "8350": "your password of port 8350",
+        "8351": "your password of port 8351",
+        "8352": "your password of port 8352"
+    },
+    "timeout": 60,
+    "udp_timeout": 60,
+    "method": "aes-256-cfb",
+    "obfs": "tls1.2_ticket_auth",
+    "obfs_param": "baidu.com,163.com,qq.com",
+    "protocol": "auth_sha1_v4",
+    "protocol_param": ""
+}' > /etc/shadowsocksr-libev/config.json;
+fi
+
+# add user
+cat /etc/passwd | grep $USER ;
+if [ 0 -ne $? ]; then
+    sudo useradd $USER -M -s /sbin/nologin ;
+fi
+
+# systemd and firewall
+if [ -e "/usr/lib/systemd/system" ] && [ ! -e "/usr/lib/systemd/system/shadowsocksr-libev.service" ]; then
+    sudo cp shadowsocksr-libev.service "/usr/lib/systemd/system/shadowsocksr-libev.service" -f;
+    systemctl enable kcptun ;
+    systemctl restart kcptun ;
+fi
+
+if [ -e "/usr/lib/firewalld/services" ] && [ ! -e "/usr/lib/firewalld/services/shadowsocksr-libev.xml" ]; then
+    echo "setup firewalld";
+    sudo echo '<?xml version="1.0" encoding="utf-8"?>
+<service>
+  <short>shadowsocksr-libev</short>
+  <description>shadowsocksr-libev.</description>
+  <port protocol="tcp" port="8350"/>
+  <port protocol="udp" port="8350"/>
+  <port protocol="tcp" port="8351"/>
+  <port protocol="udp" port="8351"/>
+  <port protocol="tcp" port="8352"/>
+  <port protocol="udp" port="8352"/>
+</service>' > "/usr/lib/firewalld/services/shadowsocksr-libev.xml" ;
+    sudo firewall-cmd --permanent --add-service=shadowsocksr-libev ;
+    sudo firewall-cmd --reload ;
+fi
--- shadowsock&kcptun/centos_7_kcptun.sh
+++ shadowsock&kcptun/centos_7_kcptun.sh
@@ -4,33 +4,55 @@
 
 KCPTUN_HOME=/home/kcptun ;
 KCPTUN_SYSTEMD=/usr/lib/systemd/system/kcptun.service ;
-KCPTUN_URL=https://github.com/xtaci/kcptun/releases/download/v20161207/kcptun-linux-amd64-20161207.tar.gz ;
-KCPTUN_PORT=8381 ;
-KCPTUN_SS_LOCAL=127.0.0.1:8380 ;
+KCPTUN_URL=https://github.com/xtaci/kcptun/releases/download/v20170525/kcptun-linux-amd64-20170525.tar.gz ;
+KCPTUN_PORTS=(8350 8351 8352) ;
+KCPTUN_PORT_OFF=1000 ;
+KCPTUN_SS_LOCAL=127.0.0.1 ;
 
 mkdir -p "$KCPTUN_HOME/log";
 wget "$KCPTUN_URL" -O "$KCPTUN_HOME/kcptun-linux-amd64.tar.gz" ;
 
 tar -axvf "$KCPTUN_HOME/kcptun-linux-amd64.tar.gz" -C "$KCPTUN_HOME" ;
 
-echo "[Unit]
-Description=kcptun services
+function gen_kcptun_port() {
+    KCPTUN_PORT=$1;
+    let LOCAL_PORT=$KCPTUN_PORT+$KCPTUN_PORT_OFF;
+    echo "[Unit]
+Description=kcptun-$KCPTUN_PORTS services
 After=syslog.target network.target
 
 [Service]
 Type=simple
-ExecStart=$KCPTUN_HOME/server_linux_amd64 -l :$KCPTUN_PORT -t $KCPTUN_SS_LOCAL --crypt none --mtu 1350 --nocomp --mode fast --dscp 46 --log $KCPTUN_HOME/log/kcptun.log
+ExecStart=$KCPTUN_HOME/server_linux_amd64 -l :$KCPTUN_PORT -t $KCPTUN_SS_LOCAL:$LOCAL_PORT --crypt none --mtu 1350 --nocomp --mode fast --dscp 46 --log $KCPTUN_HOME/log/kcptun-$KCPTUN_PORTS.log
 ExecStop=/bin/kill -s QUIT \$MAINPID
 PrivateTmp=true
 Restart=on-failure
 SuccessExitStatus=SIGTERM
 
 [Install]
-WantedBy=multi-user.target" > /usr/lib/systemd/system/kcptun.service ;
+WantedBy=multi-user.target" > /usr/lib/systemd/system/kcptun-$KCPTUN_PORT.service ;
 
-systemctl enable kcptun ;
-systemctl start kcptun ;
+    systemctl enable kcptun-$KCPTUN_PORT ;
+    systemctl start kcptun-$KCPTUN_PORT ;
 
-firewall-cmd --permanent --add-port=$KCPTUN_PORT/tcp ;
-firewall-cmd --permanent --add-port=$KCPTUN_PORT/udp ;
-firewall-cmd --reload ;
+    firewall-cmd --permanent --add-port=$KCPTUN_PORT/tcp ;
+    firewall-cmd --permanent --add-port=$KCPTUN_PORT/udp ;
+    firewall-cmd --reload ;
+
+    if [ -e "/usr/lib/firewalld/services" ] && [ ! -e "/usr/lib/firewalld/services/kcptun-$KCPTUN_PORT.xml" ]; then
+        echo "setup firewalld";
+        sudo echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>
+    <service>
+    <short>kcptun-$KCPTUN_PORT</short>
+    <description>kcptun-$KCPTUN_PORT</description>
+    <port protocol=\"tcp\" port=\"$KCPTUN_PORT\"/>
+    <port protocol=\"udp\" port=\"$KCPTUN_PORT\"/>
+    </service>" > "/usr/lib/firewalld/services/kcptun-$KCPTUN_PORT.xml" ;
+        sudo firewall-cmd --permanent --add-service=kcptun-$KCPTUN_PORT ;
+        sudo firewall-cmd --reload ;
+    fi
+}
+
+for PORT in ${KCPTUN_PORTS[@]}; do
+    gen_kcptun_port $PORT;
+done
