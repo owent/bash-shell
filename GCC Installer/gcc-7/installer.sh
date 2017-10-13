9490e34bda8b73a2873253fef446e109fdef2409
owent <owt5008137@live.com>
2017-10-13 12:06:44 +0800

    移除垃圾回收选项，老一点的gcc不支持这个选项，反正我测试的时候也没感觉出来有什么用

--- GCC Installer/gcc-7/installer.sh
+++ GCC Installer/gcc-7/installer.sh
@@ -352,7 +352,8 @@ if [ -z "$BUILD_TARGET_COMPOMENTS" ] || [ "0" == $(is_in_list gcc $BUILD_TARGET_
         cd objdir;
         # ======================= 这一行的最后一个参数请注意，如果要支持其他语言要安装依赖库并打开对该语言的支持 =======================
         GCC_CONF_OPTION_ALL="--prefix=$PREFIX_DIR --with-gmp=$PREFIX_DIR --with-mpc=$PREFIX_DIR --with-mpfr=$PREFIX_DIR --with-isl=$PREFIX_DIR $BDWGC_PREBIUILT --enable-bootstrap --enable-build-with-cxx --disable-libjava-multilib --enable-checking=release --enable-gold --enable-ld --enable-libada --enable-libssp --enable-lto --enable-objc-gc --enable-vtable-verify --enable-shared --enable-static --enable-gnu-unique-object --enable-linker-build-id $GCC_OPT_DISABLE_MULTILIB $BUILD_TARGET_CONF_OPTION";
-        env CFLAGS="--ggc-min-expand=0 --ggc-min-heapsize=6291456" CXXFLAGS="--ggc-min-expand=0 --ggc-min-heapsize=6291456" ../$GCC_DIR/configure $GCC_CONF_OPTION_ALL ;
+        # env CFLAGS="--ggc-min-expand=0 --ggc-min-heapsize=6291456" CXXFLAGS="--ggc-min-expand=0 --ggc-min-heapsize=6291456" 老版本的gcc没有这个选项
+        ../$GCC_DIR/configure $GCC_CONF_OPTION_ALL ;
         make $BUILD_THREAD_OPT && make install;
         cd "$WORKING_DIR";
 
