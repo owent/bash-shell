1ab88bb471f1f087f596df681192153d7c5b11a7
owent <owt5008137@live.com>
2018-07-26 15:55:00 +0800

    增加可通过环境变量设置aria2的部分参数

--- Others/aria2-rpc.ps1
+++ Others/aria2-rpc.ps1
@@ -1,24 +1,50 @@
-if (-Not (Test-Path "log")) {
-    mkdir log
-}
-if (-Not (Test-Path "download")) {
-    mkdir download
-}
-if (-Not (Test-Path "session")) {
-    mkdir session
+Write-Output "Try loading environment ARIA2_HOME,ARIA2_BIN,ARIA2_CONF ...";
+
+if ([string]::IsNullOrEmpty($Env:ARIA2_HOME)) {
+    $Env:ARIA2_HOME = Split-Path $MyInvocation.MyCommand.Path
 }
 
-if (Test-Path "session/aria2.session") {
-    aria2c --conf-path=aria2.conf --daemon --input-file=session/aria2.session
-}
-else {
-    aria2c --conf-path=aria2.conf --daemon
+if ([string]::IsNullOrEmpty($Env:ARIA2_BIN)) {
+    $Env:ARIA2_BIN = "aria2c"
 }
 
-# aria2c --conf-path=aria2.conf;
+if ([string]::IsNullOrEmpty($Env:ARIA2_CONF)) {
+    if (Test-Path "$Env:ARIA2_HOME/etc/aria2.conf") {
+        $Env:ARIA2_CONF = "$Env:ARIA2_HOME/etc/aria2.conf";
+    }
+    elseif (Test-Path "$Env:ARIA2_HOME/conf/aria2.conf") {
+        $Env:ARIA2_CONF = "$Env:ARIA2_HOME/conf/aria2.conf";
+    }
+    else {
+        $Env:ARIA2_CONF = "$Env:ARIA2_HOME/aria2.conf";
+    }
+}
+
+if (-Not (Test-Path "$Env:ARIA2_HOME/log")) {
+    mkdir "$Env:ARIA2_HOME/log"
+}
+if (-Not (Test-Path "$Env:ARIA2_HOME/download")) {
+    mkdir "$Env:ARIA2_HOME/download"
+}
+if (-Not (Test-Path "$Env:ARIA2_HOME/session")) {
+    mkdir "$Env:ARIA2_HOME/session"
+}
 
 # client
 # https://github.com/ziahamza/webui-aria2
 #   https://ziahamza.github.io/webui-aria2/
 # https://github.com/mayswind/AriaNg
 #   http://ariang.mayswind.net
+
+Write-Output "Clients: "
+Write-Output "    WebUI-Aria2: https://ziahamza.github.io/webui-aria2/"
+Write-Output "    AriaNg:      http://ariang.mayswind.net"
+
+if (Test-Path "$Env:ARIA2_HOME/session/aria2.session") {
+    Write-Output "Run: $Env:ARIA2_BIN --conf-path=$Env:ARIA2_CONF --daemon --input-file=$Env:ARIA2_HOME/session/aria2.session"
+    . $Env:ARIA2_BIN --conf-path=$Env:ARIA2_CONF --daemon --input-file=$Env:ARIA2_HOME/session/aria2.session
+}
+else {
+    Write-Output "Run: $Env:ARIA2_BIN --conf-path=$Env:ARIA2_CONF --daemon"
+    . $Env:ARIA2_BIN --conf-path=$Env:ARIA2_CONF --daemon
+}
--- Others/aria2-rpc.sh
+++ Others/aria2-rpc.sh
@@ -1,15 +1,50 @@
-mkdir -p log;
-mkdir -p download;
-mkdir -p session;
-if [ -e "session/aria2.session" ]; then
-    aria2c --conf-path=aria2.conf --daemon --input-file=session/aria2.session;
-else
-    aria2c --conf-path=aria2.conf --daemon;
+#!/bin/bash
+
+echo "Try loading environment ARIA2_HOME,ARIA2_BIN,ARIA2_CONF ...";
+
+if [ -z "$ARIA2_HOME" ]; then
+    export ARIA2_HOME="$(dirname $0)";
 fi
-# aria2c --conf-path=aria2.conf;
+
+if [ -z "$ARIA2_BIN" ]; then
+    export ARIA2_BIN="aria2c";
+fi
+
+if [ -z "$ARIA2_CONF" ]; then
+    if [ -e "$ARIA2_HOME/etc/aria2.conf" ]; then
+        export ARIA2_CONF="$ARIA2_HOME/etc/aria2.conf";
+    elif [ -e "$ARIA2_HOME/conf/aria2.conf" ]; then
+        export ARIA2_CONF="$ARIA2_HOME/conf/aria2.conf";
+    else
+        export ARIA2_CONF="$ARIA2_HOME/aria2.conf";
+    fi
+fi
+
+if [ ! -e "$ARIA2_CONF" ]; then
+    echo "aria2 configure file $ARIA2_CONF not found.";
+    exit 1;
+fi
+
+mkdir -p "$ARIA2_HOME/log";
+mkdir -p "$ARIA2_HOME/download";
+mkdir -p "$ARIA2_HOME/session";
 
 # client
 # https://github.com/ziahamza/webui-aria2
 #   https://ziahamza.github.io/webui-aria2/
 # https://github.com/mayswind/AriaNg
 #   http://ariang.mayswind.net
+
+echo "Clients:";
+echo "    WebUI-Aria2: https://ziahamza.github.io/webui-aria2/";
+echo "    AriaNg:      http://ariang.mayswind.net/";
+
+if [ -e "$ARIA2_HOME/session/aria2.session" ]; then
+    echo "Run: $ARIA2_BIN --conf-path=$ARIA2_CONF --daemon --input-file=$ARIA2_HOME/session/aria2.session;";
+    chmod +x "$ARIA2_BIN";
+    $ARIA2_BIN --conf-path=$ARIA2_CONF --daemon --input-file=$ARIA2_HOME/session/aria2.session;
+else
+    echo "Run: $ARIA2_BIN --conf-path=$ARIA2_CONF --daemon;";
+    chmod +x "$ARIA2_BIN";
+    $ARIA2_BIN --conf-path=$ARIA2_CONF --daemon;
+fi
