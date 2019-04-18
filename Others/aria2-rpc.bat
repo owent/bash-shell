a1450f31b2bb0eec2e970cd68418a789bb8659d5
owent <admin@owent.net>
2019-04-18 22:57:24 +0800

    fix bat

--- Others/aria2-rpc.bat
+++ Others/aria2-rpc.bat
@@ -1,2 +1,17 @@
 @echo off
-@powershell -File aria2-rpc.ps1
+
+chcp 65001
+
+where pwsh
+
+if ERRORLEVEL 0 goto USE_POWERSHELL_CORE
+
+exit %ERRORLEVEL%
+
+:USE_POWERSHELL
+
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Interactive -NoExit -File %~dp0\aria2-rpc.ps1
+
+:USE_POWERSHELL_CORE
+
+@pwsh.exe -NoProfile -InputFormat None -ExecutionPolicy Bypass -Interactive -NoExit -File %~dp0\aria2-rpc.ps1
