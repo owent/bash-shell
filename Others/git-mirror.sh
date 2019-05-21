228c8f4894ef7230380090c757a7c93d963fabd3
OWenT <admin@owent.net>
2019-05-21 12:42:13 +0800

    Update git-mirror.sh

--- Others/git-mirror.sh
+++ Others/git-mirror.sh
@@ -7,6 +7,13 @@ git push --mirror https://gitlab.com/atframework/atframe_utils.git
 
 更新同步仓库
 ============
-cd UnrealEngine
+cd atframe_utils
 git remote update
 git push --mirror https://gitlab.com/atframework/atframe_utils.git
+
+手动指定更新的引用
+============
+cd atframe_utils
+git remote update
+# git show-ref --head
+git push --force https://gitlab.com/atframework/atframe_utils.git "+refs/heads/*:refs/heads/*" "+refs/tags/*:refs/tags/*"
