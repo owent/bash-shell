ee9cbff1deff7b5a866414a6cc0a1dc311134afb
OWenT <admin@owent.net>
2019-09-04 19:57:16 +0800

    Update centos_7_setup_v2ray_server.sh

--- v2ray/centos_7_setup_v2ray_server.sh
+++ v2ray/centos_7_setup_v2ray_server.sh
@@ -17,7 +17,7 @@ if [ -z "$V2RAY_USER" ]; then
     V2RAY_USER="v2ray";
 fi
 if [ -z "$VERSION" ]; then
-    VERSION="4.19.1";
+    VERSION="4.20.0";
 fi
 if [ -z "$ARCH" ]; then
     ARCH="linux-64";
